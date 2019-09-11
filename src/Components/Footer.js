6f3ab850a7246b60e73aa42e69bae8a2d5a764ce
Pascale Pierre <pascaleee@gmail.com>
2019-09-10 21:58:22 -0500

    added axios and call to move North

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import '../App.css';
 import {Button} from 'primereact/button';
+import axios from "axios";
 
   export class Footer extends Component {
     constructor() {
         super();
         this.state = {
-      }};
+      }
+    
+    };
 
       render(){
+  
+        function move(direction, prediction){
+axios.post("http://localhost:5000/move", {"dir": direction, "predict": prediction}).then((res)=>{console.log(res)},(error)=>{console.log(error)})
+        }
+    
+          function handleClick(e) {
+            e.preventDefault();
+            console.log('The link was clicked.');
+          }
+        
         return(
           <div className='footer'>
               <div className='explore'>
@@ -18,14 +31,13 @@ import {Button} from 'primereact/button';
               <h2>Message</h2>
               </div>
               <div className='controls'>
-      
-              <div className="buttom" >N</div>
-              <div className="buttom" >S</div>
-              <div className="buttom" >E</div>
-              <div className="buttom" >W</div>
-              <i className="buttom" class="fas fa-store"></i>
-              <i className="buttom" class="fas fa-dollar-sign"></i>
-              <i className="buttom" class="fas fa-hand-holding"></i>
+              <div onClick={ ()=> move('n', this.state.north)} className="buttom" >N</div>
+              <div onClick={handleClick} className="buttom" >S</div>
+              <div onClick={handleClick} className="buttom" >E</div>
+              <div onClick={handleClick} className="buttom" >W</div>
+              <i onClick={handleClick} className="buttom" class="fas fa-store"></i>
+              <i onClick={handleClick} className="buttom" class="fas fa-dollar-sign"></i>
+            
               
               </div>
           </div>
