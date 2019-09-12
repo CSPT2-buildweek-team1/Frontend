8f8c88ac8df5675ca57d30ac7e3b520e5d1a05a3
BrandonMoll <brandonmoll85@gmail.com>
2019-09-11 20:49:51 -0500

    move buttons working

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,28 +4,48 @@ import {Button} from 'primereact/button';
 import axios from "axios";
 
   export class Footer extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
+          room_id: 0,
+          dir: {
+            n: 0,
+            e: 0,
+            w: 0,
+            s: 0
+          }
+        }
       }
-    
-    };
+
+    move = (direction, prediction) => {
+      axios
+        .post("http://localhost:5000/move", 
+          {"dir": direction, "predict": prediction})
+        .then((res)=>{
+            console.log(res)
+            let room_id = res.data.data.room_id;
+            let exits = res.data.exits
+            this.setState({...this.state, room_id: room_id, dir: exits});
+            })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+
+    sell = (item) => {
+      axios.post("http://localhost:5000/sell", {"item": item }).then((res)=>{console.log(res)},(error)=>{console.log(error)})}
+
+    take = (item) => {
+      axios.post("http://localhost:5000/take", {"item": item }).then((res)=>{console.log(res)},(error)=>{console.log(error)})}
+  
+    handleClick = (e) => {
+        e.preventDefault();
+        console.log('The link was clicked.');
+      }
+
 
       render(){
-  
-        function move(direction, prediction){
-          axios.post("http://localhost:5000/move", {"dir": direction, "predict": prediction}).then((res)=>{console.log(res)},(error)=>{console.log(error)})
-        }
-        function sell(item){
-          axios.post("http://localhost:5000/sell", {"item": item }).then((res)=>{console.log(res)},(error)=>{console.log(error)})}
-    
-        function take(item){
-          axios.post("http://localhost:5000/take", {"item": item }).then((res)=>{console.log(res)},(error)=>{console.log(error)})}
-      
-          function handleClick(e) {
-            e.preventDefault();
-            console.log('The link was clicked.');
-          }
+        console.log('STATE', this.state)
         
         return(
           <div className='footer'>
@@ -37,12 +57,12 @@ import axios from "axios";
               </div>
               <div className='controls'>
 
-              <div onClick={ ()=> move('n', this.state.north)} className="button" >N</div>
-              <div onClick={ ()=> move('s', this.state.south)} className="button" >S</div>
-              <div onClick={ ()=> move('e', this.state.east)} className="button" >E</div>
-              <div onClick={ ()=> move('w', this.state.west)} className="button" >W</div>
-              <i onClick={ ()=> sell('item', this.state.item)} className="button" class="fas fa-store"></i>
-              <i onClick={ ()=> take('item', this.state.item)} className="button" class="fas fa-dollar-sign"></i>
+              <div onClick={ ()=> this.move('n', this.state.north)} className="button" >N</div>
+              <div onClick={ ()=> this.move('s', this.state.south)} className="button" >S</div>
+              <div onClick={ ()=> this.move('e', this.state.east)} className="button" >E</div>
+              <div onClick={ ()=> this.move('w', this.state.west)} className="button" >W</div>
+              <i onClick={ ()=> this.sell('item', this.state.item)} className="button" class="fas fa-store"></i>
+              <i onClick={ ()=> this.take('item', this.state.item)} className="button" class="fas fa-dollar-sign"></i>
             
 
               
