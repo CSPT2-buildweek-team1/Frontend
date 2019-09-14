332dc2157c0cff77c71b64894450f9861ea628ad
Pascale Pierre <pascaleee@gmail.com>
2019-09-14 11:35:32 -0500

    added player to bottom sidebar formatted direction in sidebar changed font for message

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -9,7 +9,7 @@ import React, {Component} from 'react';
 
       render(){
 
-        console.log('ROOM', this.props.room.room_id, 'EXITS',this.props.room.exits)
+        console.log('ROOM', this.props.room.room_id, 'EXITS',this.props.room.exits, 'PLAYER',this.props.player)
 
         return(
         <div className='sidebar'>
@@ -20,16 +20,17 @@ import React, {Component} from 'react';
                 </div>
                 <h2>{this.props.room.title}</h2>
                 <h2>Exits: </h2>
-                <p><span>{this.props.room.exits.n ? <p>N: {this.props.room.exits.n}</p> : null}</span>
-                <span>{this.props.room.exits.s ? <p>S: {this.props.room.exits.s}</p> : null}</span>
-                <span>{this.props.room.exits.e ? <p>E: {this.props.room.exits.e}</p> : null}</span>
-                <span>{this.props.room.exits.w ? <p>W: {this.props.room.exits.w}</p> : null}</span></p>
+                <p className='exits'><span className='exit' >{this.props.room.exits.n ? <p>N: {this.props.room.exits.n}</p> : null}</span>
+                <span className='exit'>{this.props.room.exits.s ? <p>S: {this.props.room.exits.s}</p> : null}</span>
+                <span className='exit'>{this.props.room.exits.e ? <p>E: {this.props.room.exits.e}</p> : null}</span>
+                <span className='exit'>{this.props.room.exits.w ? <p>W: {this.props.room.exits.w}</p> : null}</span></p>
                 <h2>Items:</h2>
                 {this.props.room.items.forEach(item => <p>{item}</p>)}
 
             </div>
             <div className='bottom'>
-                <h2>BOTTOM</h2>
+                <h2>{this.props.player.name}</h2>
+                
             </div>
         </div>
         )
